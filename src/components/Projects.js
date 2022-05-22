a8c5d1338f30e6f63fb071506708dba360d84d1b
Jakob Edelstein <jakobedelstein@gmail.com>
2022-05-22 11:52:39 -0600

    render projects dynamically

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,31 @@
 
-function Projects(props) {
+import PortfolioListItem from './PortfolioListItem';
+import { portfolioItems } from '../utils/constants.js'
+
+console.log(portfolioItems)
+
+const Projects = () => {
+
+  const renderPortfolioItem = ({
+    projectUrl,
+    linkText,
+    linkDescription
+  }) => {
+    return (
+      <PortfolioListItem 
+      to={projectUrl}
+      linkText={linkText}
+      linkDescription={linkDescription}
+    />
+    )
+  }
+
   return (
      <>
         <div>
             <h2>Projects</h2>
             <ul className="project-list">
-                <li><a href="https://github.com/jake-edel/ce_console.net_4.6.1">A cost estimation program</a> to calculate the price of a stair tower based on top level parameters. Written in C# for Method Stair System.</li>
-                <br/><br/>
-                <li><a href="https://github.com/jake-edel/freecad-py">Python scripting</a> to extract part parameters from a CAD model and use it to configure part designs for fabrication and assembly.</li>
-                <br/><br/>
-                <li><a href="https://github.com/jake-edel/enigma">Enigma</a>: A two way encryption algorithim, written in Ruby, to encode/decode a message using a key and date.</li>
-                <br/><br/>
-                <li><a href="https://github.com/jake-edel/rock-paper-scissors">Sword, Shield, Magic</a>: A riff on a browser based Rock, Paper, Scissors written in HTML/CSS/Javascript.</li>
+              {portfolioItems.map(renderPortfolioItem)}
             </ul>
         </div>
      </> 
