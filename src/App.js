7f3da33637cc008a9086e551e1dfbe7c45e56567
Jakob Edelstein <jakobedelstein@gmail.com>
2022-05-22 09:10:08 -0600

    edit layout into two columns

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import './App.css';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Navbar from './components/Navbar';
 import Resume from './components/Resume';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <main>
-      <Router>
-        <Routes>
-          <Route path='/' exact element={<Navbar/>}> </Route>
-          <Route path='/about' exact element={<About/>}></Route>
-          <Route path='/projects' exact element={<Projects/>}></Route>
-          <Route path='/contact' exact element={<Contact/>}></Route>
-          <Route path='/resume' exact element={<Resume/>}></Route>
-        </Routes>
-    </Router>
-    </main>
+      <div className='sidebar'>
+        {/* <img src={headshot} className='headshot' alt='Jakob Edelstein Headshot'></img> */}
+        <div className='mobile-spacer'>
+          <div className='profile-image-container'></div>
+        </div>
+      </div>
+
+        
+      <div className='content'>
+          <h1 className='name'>Jakob Edelstein</h1>
+        <div className='page-content'>
+          <Router>
+              <Navbar/>
+            <Routes>
+              <Route path='/' exact element={<About/>}> </Route>
+              <Route path='/projects' exact element={<Projects/>}></Route>
+              <Route path='/contact' exact element={<Contact/>}></Route>
+              <Route path='/resume' exact element={<Resume/>}></Route>
+            </Routes>
+        </Router>
+      </div>
+    </div>
+  </main>
   );
 }
 
