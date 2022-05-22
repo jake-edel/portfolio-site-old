675f39c819ebae3db68232a2341f1a2804b2f83f
Jakob Edelstein <jakobedelstein@gmail.com>
2022-05-22 11:48:03 -0600

    move project data to ext file

---
+++ src/utils/constants.js
@@ -0,0 +1,27 @@
+export const portfolioItems = [
+    {
+        projectUrl: 'https://github.com/jake-edel/ce_console.net_4.6.1',
+        linkText: 'A cost estimation program ',
+        linkDescription: ' to calculate the price of a stair tower based on top level parameters. Written in C# for Method Stair System.'
+    },
+    {
+        projectUrl: 'https://github.com/jake-edel/freecad-py',
+        linkText: 'Python scripting',
+        linkDescription: ' to extract part parameters from a CAD model and use it to configure part designs for fabrication and assembly.'
+    },
+    {
+        projectUrl: 'https://github.com/jake-edel/enigma',
+        linkText: 'Enigma',
+        linkDescription: ': A two way encryption algorithim, written in Ruby, to encode/decode a message using a key and date.'
+    },
+    {
+        projectUrl: 'https://github.com/jake-edel/rock-paper-scissors',
+        linkText: 'Sword, Shield, Magic',
+        linkDescription: ': A riff on a browser based Rock, Paper, Scissors written in HTML/CSS/Javascript.'
+    },
+    {
+        projectUrl: 'https://github.com/jake-edel/battleship',
+        linkText: 'Battleship',
+        linkDescription: ': A console based version of the classic game Battleship, written in Ruby'
+    },
+]
