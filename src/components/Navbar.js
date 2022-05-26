d110e1893ea1608158582f411c93a2df2a1352f4
Jakob Edelstein <jakobedelstein@gmail.com>
2022-05-26 15:36:22 -0600

    remove test from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom'
-
+import CustomLink from './CustomLink';
 
 const Navbar = () => {
     return (
     <div>
         <div className='container'>
             <ul className='navbar'>
-                <li className='navlink'><Link to='/'>About</Link></li>
-                <li className='navlink'><Link to='/projects'>Projects</Link></li>
-                <li className='navlink'><Link to='/resume'>Resume</Link></li>
-                <li className='navlink'><Link to='/contact'>Contact</Link></li>
+                <li className='navlink'><CustomLink to='/' children='About'></CustomLink></li>
+                <li className='navlink'><CustomLink to='/projects' children='Projects'></CustomLink></li>
+                <li className='navlink'><CustomLink to='/resume' children='Resume'></CustomLink></li>
+                <li className='navlink'><CustomLink to='/contact' children='Contact'></CustomLink></li>
+                {/* <li className='navlink'><CustomLink to='/test' children='Test'></CustomLink></li> */}
             </ul>
         </div>
     </div>
