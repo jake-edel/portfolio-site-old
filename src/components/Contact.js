f9cb29fcd93fa77e9554e8a847663c9345903e71
Jakob Edelstein <jakobedelstein@gmail.com>
2022-05-27 08:10:24 -0600

    add github link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,8 @@ const Contact = () => {
                 <h3>jakobedelstein@gmail.com</h3>
                 <h3>(516)459-3979</h3>
                 <a href="https://www.linkedin.com/in/jakob-edel/">LinkedIn</a>
+                <br/><br/>
+                <a href="https://www.github.com/jake-edel/">GitHub</a>
             </div>
         </>)
 }
